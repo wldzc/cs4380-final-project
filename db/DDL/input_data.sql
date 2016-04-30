569438f68db204fc3e1173989300d0d705586c5b
Tony Forsythe <forsythetony@gmail.com>
2016-04-30 13:48:32 -0500

    Adds the load data statement for the life_period table

--- db/DDL/input_data.sql
+++ db/DDL/input_data.sql
@@ -14,3 +14,13 @@ SET
 	start_date 	= STR_TO_DATE(@start_date, '%c-%e-%Y'),
 	end_date	= STR_TO_DATE(@end_date, '%c-%e-%Y')
 ;
+
+
+#	Load data for the life_period table
+LOAD DATA LOCAL INFILE '../input_data/life_period.csv' INTO TABLE life_period
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+( name , start_year , end_year );
+
